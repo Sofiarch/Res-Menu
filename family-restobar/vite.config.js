a336166d3c565bd4688ab87ce0579f6d0d2d48a9
Sofi <sofiarchlinux@gmail.com>
2025-12-09 04:11:14 +0300

    Setup Deployment config

---
+++ family-restobar/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/family-restobar/", // <--- ADD THIS LINE (Replace with your actual repo name)
+})
