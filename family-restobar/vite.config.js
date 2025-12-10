24b341443b9c385cbd87f07f8c300b091168c85a
Sofi <sofiarchlinux@gmail.com>
2025-12-10 19:07:39 +0300

    Delete family-restobar directory

--- family-restobar/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/family-restobar/", // <--- ADD THIS LINE (Replace with your actual repo name)
-})
